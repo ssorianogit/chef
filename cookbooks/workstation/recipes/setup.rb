bdeb785511b8dc2313420a47165455fb21e13ab2
samuel.soriano <samuel.soriano@valantmed.com>
2017-10-28 17:28:38 +0200

    Add the firts cookbook

---
+++ cookbooks/workstation/recipes/setup.rb
@@ -0,0 +1,22 @@
+service 'ntpd' do
+    action [ :enable, :start ]
+end
+
+package 'Intall tree' do
+    package_name 'ntp'
+    action :install
+end
+
+file 'Create motd file' do
+    path '/etc/motd'
+    content 'This Server it is property of Samuel Soriano'
+    owner 'root'
+    group 'root'
+    action :create
+
+end
+
+package 'Intall Git' do
+    package_name 'git'
+    action :install
+end
