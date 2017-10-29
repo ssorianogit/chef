9aac676e383aa3b5527f707ec8ec8f826a04f930
Chef server <samuelsc@gmail.com>
2017-10-29 09:00:59 +0000

    Add new cookbook for install apache

---
+++ cookbooks/apache/recipes/server.rb
@@ -0,0 +1,12 @@
+package 'Install Apache service' do
+  package_name 'httpd'
+end
+
+service 'httpd' do
+  action [ :enable, :start]
+end
+
+file '/var/www/html/index.html' do
+ content '<h1>Hello, world!</h1>'
+ action 'create'
+end
